0ad96f9ef6f8fa8549a23319f2e84df6d2f60962
Joel Jani <joel.luca.jani@gmail.com>
2019-10-25 12:55:25 +0200

    Add Grid50 and small fixes

---
+++ src/main/java/jdraw/grid/Grid20.java
@@ -0,0 +1,51 @@
+package jdraw.grid;
+
+import jdraw.framework.DrawGrid;
+
+import java.awt.*;
+
+public class Grid20 implements DrawGrid {
+
+    private int step;
+
+    @Override
+    public Point constrainPoint(Point p) {
+        System.out.println("SimpleGrid:constrainPoint: " + p);
+        return p;
+    }
+
+    @Override
+    public int getStepX(boolean right) {
+        System.out.println("getStepX called");
+        step = 20;
+        return step;
+    }
+
+    @Override
+    public int getStepY(boolean down) {
+        System.out.println("getStepY called");
+        step = 20;
+        return step;
+    }
+
+    @Override
+    public void activate() {
+        System.out.println("activate() called");
+    }
+
+    @Override
+    public void deactivate() {
+        System.out.println("deactivate() called");
+    }
+
+    @Override
+    public void mouseDown() {
+        System.out.println("mouseDown() called");
+    }
+
+    @Override
+    public void mouseUp() {
+        System.out.println("mouseUp() called");
+    }
+}
+
